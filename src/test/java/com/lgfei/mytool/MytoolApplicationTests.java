129a2b378ba458577a951161c647a10bd9a34479
long.gf <long.gf@topsports.com.cn>
2023-06-19 09:31:39 +0800

    init by https://start.aliyun.com/

---
+++ src/test/java/com/lgfei/mytool/MytoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgfei.mytool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
